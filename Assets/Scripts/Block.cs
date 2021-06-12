ab7727cbf4d38f43472247db71a8918390c0109f
Sergey Serpeiko <ckopnuc@gmail.com>
2021-06-12 18:37:27 +0300

    reproduce lesson with colors and ligtning

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class Block : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem _destroyEffect;
+
+    private MeshRenderer _meshRenderer;
+
+    public event UnityAction<Block> BulletHit;
+
+    private void Awake()
+    {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void SetColor(Color color)
+    {
+        _meshRenderer.material.color = color;
+    }
+
+    public void Break()
+    {
+        BulletHit?.Invoke(this);
+        ParticleSystemRenderer renderer = Instantiate(_destroyEffect, transform.position, _destroyEffect.transform.rotation).GetComponent<ParticleSystemRenderer>();
+        renderer.material.color = GetComponent<MeshRenderer>().material.color;
+        Destroy(gameObject);
+    }
+}
